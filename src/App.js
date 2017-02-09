0c8aec50a4411206a33e850ef6c4598c9a660ad9
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-09 17:21:36 -0500

    relearing the props key word

--- src/App.js
+++ src/App.js
@@ -16,7 +16,41 @@ const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
 `;
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
 
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+             src={props.author.avatarUrl}
+             alt={props.author.name} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
 // console.log(`Hello baby girl`);
 class App extends Component {
   render() {
@@ -31,6 +65,13 @@ class App extends Component {
         </p>
         <Wrapper>
         <Title>Hello World, this is my first styled component!</Title>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />
         </Wrapper>
 
       </div>
