89fff5daccd526e296002414b37c9f33a4059e12
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-09 17:00:22 -0500

    added styled componets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import styled from 'styled-components';
 //import image from 'from name of image'
 import './App.css';
+
+
+const Title=styled.h1`
+
+          font-size=1.5em;
+          text-align:center;
+          color:palevioletred;
+          `;
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 // console.log(`Hello baby girl`);
 class App extends Component {
   render() {
@@ -14,6 +29,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Wrapper>
+        <Title>Hello World, this is my first styled component!</Title>
+        </Wrapper>
+
       </div>
     );
   }
