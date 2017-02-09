7ab1597a965449b2cc2f5a680677df88e3690d6f
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-09 17:33:27 -0500

    added a simple logic button

--- src/App.js
+++ src/App.js
@@ -53,6 +53,16 @@ const comment = {
 };
 // console.log(`Hello baby girl`);
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
   render() {
     return (
       <div className="App">
@@ -74,9 +84,12 @@ class App extends Component {
     author={comment.author} />
         </Wrapper>
         <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div> 
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
 
       </div>
     );
