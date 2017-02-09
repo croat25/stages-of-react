982098768644039f743d654c02038692b1ac6258
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-09 16:46:23 -0500

    added webpack babel and es2016

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 //import image from 'from name of image'
 import './App.css';
-
+// console.log(`Hello baby girl`);
 class App extends Component {
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack');
+const path=require('path');
+
+const config = {
   	entry: './src/App.js',
 	output:{
 		path:path.resolve(__dirname,"firstapp"),
 		filename:'myfirstwepback.bundle.js'
-	}
+	},
+	module:{
+		rules:[
+		{test:'/\.(js|jsx)$/',exclude:'/node_modules/',loader :'babel-loader'}
+		]
+	},
+	plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
+
+module.exports = config;
